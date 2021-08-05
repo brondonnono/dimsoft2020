380c69189cff568961689fa107e7c5e88e54e413
brondonnono <brondonnono3@gmail.com>
2021-08-05 09:40:23 +0100

    function write

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-		// document.getElementById('title').style.color = 'blue';
+	function style(){
+        // document.getElementById('title').style.color = 'blue';
         document.getElementsByTagName("h1")[0].style.color = 'blue';
+    }
