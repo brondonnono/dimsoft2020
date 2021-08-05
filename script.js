92988998ec0b4fe41f3be774ffa2540f63ade348
brondonnono <brondonnono3@gmail.com>
2021-08-05 09:45:13 +0100

    adding script on head ok

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-	function style(){
+	function style() {
         // document.getElementById('title').style.color = 'blue';
         document.getElementsByTagName("h1")[0].style.color = 'blue';
     }
